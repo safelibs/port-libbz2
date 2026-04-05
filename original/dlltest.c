f1fac70e4f512bab0b324b4ab6b7d88b5cfc8f0e
Yan <yans@yancomm.net>
2026-04-05 10:32:35 -0700

    Rewrite dlltest to use official APIs

--- original/dlltest.c
+++ original/dlltest.c
@@ -1,6 +1,6 @@
 /*
    minibz2
-      libbz2.dll test program.
+      libbz2.dll test program using the official streaming APIs.
       by Yoshioka Tsuneo (tsuneo@rr.iij4u.or.jp)
       This file is Public Domain.  Welcome any email to me.
 
@@ -11,14 +11,10 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include "bzlib.h"
-#ifdef _WIN32
-#include <io.h>
-#endif
-
 
 #ifdef _WIN32
 
-#define BZ2_LIBNAME "libbz2-1.0.2.DLL" 
+#define BZ2_LIBNAME "libbz2.dll"
 
 #include <windows.h>
 static int BZ2DLLLoaded = 0;
@@ -27,46 +23,77 @@ int BZ2DLLLoadLibrary(void)
 {
    HINSTANCE hLib;
 
-   if(BZ2DLLLoaded==1){return 0;}
-   hLib=LoadLibrary(BZ2_LIBNAME);
-   if(hLib == NULL){
-      fprintf(stderr,"Can't load %s\n",BZ2_LIBNAME);
+   if (BZ2DLLLoaded == 1) return 0;
+   hLib = LoadLibrary(BZ2_LIBNAME);
+   if (hLib == NULL) {
+      fprintf(stderr, "Can't load %s\n", BZ2_LIBNAME);
       return -1;
    }
-   BZ2_bzlibVersion=GetProcAddress(hLib,"BZ2_bzlibVersion");
-   BZ2_bzopen=GetProcAddress(hLib,"BZ2_bzopen");
-   BZ2_bzdopen=GetProcAddress(hLib,"BZ2_bzdopen");
-   BZ2_bzread=GetProcAddress(hLib,"BZ2_bzread");
-   BZ2_bzwrite=GetProcAddress(hLib,"BZ2_bzwrite");
-   BZ2_bzflush=GetProcAddress(hLib,"BZ2_bzflush");
-   BZ2_bzclose=GetProcAddress(hLib,"BZ2_bzclose");
-   BZ2_bzerror=GetProcAddress(hLib,"BZ2_bzerror");
+   BZ2_bzReadOpen = GetProcAddress(hLib, "BZ2_bzReadOpen");
+   BZ2_bzReadClose = GetProcAddress(hLib, "BZ2_bzReadClose");
+   BZ2_bzRead = GetProcAddress(hLib, "BZ2_bzRead");
+   BZ2_bzWriteOpen = GetProcAddress(hLib, "BZ2_bzWriteOpen");
+   BZ2_bzWrite = GetProcAddress(hLib, "BZ2_bzWrite");
+   BZ2_bzWriteClose = GetProcAddress(hLib, "BZ2_bzWriteClose");
 
-   if (!BZ2_bzlibVersion || !BZ2_bzopen || !BZ2_bzdopen
-       || !BZ2_bzread || !BZ2_bzwrite || !BZ2_bzflush
-       || !BZ2_bzclose || !BZ2_bzerror) {
-      fprintf(stderr,"GetProcAddress failed.\n");
+   if (!BZ2_bzReadOpen || !BZ2_bzReadClose || !BZ2_bzRead
+       || !BZ2_bzWriteOpen || !BZ2_bzWrite || !BZ2_bzWriteClose) {
+      fprintf(stderr, "GetProcAddress failed.\n");
       return -1;
    }
-   BZ2DLLLoaded=1;
-   BZ2DLLhLib=hLib;
+   BZ2DLLLoaded = 1;
+   BZ2DLLhLib = hLib;
    return 0;
-
 }
+
 int BZ2DLLFreeLibrary(void)
 {
-   if(BZ2DLLLoaded==0){return 0;}
+   if (BZ2DLLLoaded == 0) return 0;
    FreeLibrary(BZ2DLLhLib);
-   BZ2DLLLoaded=0;
+   BZ2DLLLoaded = 0;
+   return 0;
 }
-#endif /* WIN32 */
+#endif /* _WIN32 */
 
 void usage(void)
 {
    puts("usage: minibz2 [-d] [-{1,2,..9}] [[srcfilename] destfilename]");
 }
 
-int main(int argc,char *argv[])
+static void fail_open(const char* path)
+{
+   printf("can't open [%s]\n", path);
+   perror("reason:");
+   exit(1);
+}
+
+static void fail_bzcall(const char* call, int bzerr)
+{
+   fprintf(stderr, "%s failed: %d\n", call, bzerr);
+   exit(1);
+}
+
+static FILE* open_input(const char* path)
+{
+   FILE* fp;
+
+   if (path == NULL) return stdin;
+   fp = fopen(path, "rb");
+   if (fp == NULL) fail_open(path);
+   return fp;
+}
+
+static FILE* open_output(const char* path)
+{
+   FILE* fp;
+
+   if (path == NULL) return stdout;
+   fp = fopen(path, "wb");
+   if (fp == NULL) fail_open(path);
+   return fp;
+}
+
+int main(int argc, char *argv[])
 {
    int decompress = 0;
    int level = 9;
@@ -74,98 +101,88 @@ int main(int argc,char *argv[])
    char *fn_w = NULL;
 
 #ifdef _WIN32
-   if(BZ2DLLLoadLibrary()<0){
-      fprintf(stderr,"Loading of %s failed.  Giving up.\n", BZ2_LIBNAME);
+   if (BZ2DLLLoadLibrary() < 0) {
+      fprintf(stderr, "Loading of %s failed.  Giving up.\n", BZ2_LIBNAME);
       exit(1);
    }
-   printf("Loading of %s succeeded.  Library version is %s.\n",
-          BZ2_LIBNAME, BZ2_bzlibVersion() );
+   printf("Loading of %s succeeded.\n", BZ2_LIBNAME);
 #endif
-   while(++argv,--argc){
-      if(**argv =='-' || **argv=='/'){
+   while (++argv, --argc) {
+      if (**argv == '-' || **argv == '/') {
          char *p;
 
-         for(p=*argv+1;*p;p++){
-            if(*p=='d'){
+         for (p = *argv + 1; *p; p++) {
+            if (*p == 'd') {
                decompress = 1;
-            }else if('1'<=*p && *p<='9'){
+            } else if ('1' <= *p && *p <= '9') {
                level = *p - '0';
-            }else{
+            } else {
                usage();
                exit(1);
             }
          }
-      }else{
+      } else {
          break;
       }
    }
-   if(argc>=1){
+   if (argc >= 1) {
       fn_r = *argv;
-      argc--;argv++;
-   }else{
-      fn_r = NULL;
+      argc--;
+      argv++;
    }
-   if(argc>=1){
+   if (argc >= 1) {
       fn_w = *argv;
-      argc--;argv++;
-   }else{
-      fn_w = NULL;
    }
+
    {
       int len;
+      int bzerr;
       char buff[0x1000];
-      char mode[10];
 
-      if(decompress){
-         BZFILE *BZ2fp_r = NULL;
-         FILE *fp_w = NULL;
+      if (decompress) {
+         BZFILE* bzfp_r;
+         FILE* fp_r = open_input(fn_r);
+         FILE* fp_w = open_output(fn_w);
 
-         if(fn_w){
-            if((fp_w = fopen(fn_w,"wb"))==NULL){
-               printf("can't open [%s]\n",fn_w);
+         bzfp_r = BZ2_bzReadOpen(&bzerr, fp_r, 0, 0, NULL, 0);
+         if (bzerr != BZ_OK) fail_bzcall("BZ2_bzReadOpen", bzerr);
+
+         while (1) {
+            len = BZ2_bzRead(&bzerr, bzfp_r, buff, sizeof(buff));
+            if (len > 0 && fwrite(buff, 1, len, fp_w) != (size_t)len) {
                perror("reason:");
                exit(1);
             }
-         }else{
-            fp_w = stdout;
+            if (bzerr == BZ_OK) continue;
+            if (bzerr == BZ_STREAM_END) break;
+            fail_bzcall("BZ2_bzRead", bzerr);
          }
-         if((fn_r == NULL && (BZ2fp_r = BZ2_bzdopen(fileno(stdin),"rb"))==NULL)
-            || (fn_r != NULL && (BZ2fp_r = BZ2_bzopen(fn_r,"rb"))==NULL)){
-            printf("can't bz2openstream\n");
+
+         BZ2_bzReadClose(&bzerr, bzfp_r);
+         if (bzerr != BZ_OK) fail_bzcall("BZ2_bzReadClose", bzerr);
+         if (fp_r != stdin) fclose(fp_r);
+         if (fp_w != stdout) fclose(fp_w);
+      } else {
+         BZFILE* bzfp_w;
+         FILE* fp_r = open_input(fn_r);
+         FILE* fp_w = open_output(fn_w);
+
+         bzfp_w = BZ2_bzWriteOpen(&bzerr, fp_w, level, 0, 0);
+         if (bzerr != BZ_OK) fail_bzcall("BZ2_bzWriteOpen", bzerr);
+
+         while ((len = fread(buff, 1, sizeof(buff), fp_r)) > 0) {
+            BZ2_bzWrite(&bzerr, bzfp_w, buff, len);
+            if (bzerr != BZ_OK) fail_bzcall("BZ2_bzWrite", bzerr);
+         }
+         if (ferror(fp_r)) {
+            perror("reason:");
             exit(1);
          }
-         while((len=BZ2_bzread(BZ2fp_r,buff,0x1000))>0){
-            fwrite(buff,1,len,fp_w);
-         }
-         BZ2_bzclose(BZ2fp_r);
-         if(fp_w != stdout) fclose(fp_w);
-      }else{
-         BZFILE *BZ2fp_w = NULL;
-         FILE *fp_r = NULL;
 
-         if(fn_r){
-            if((fp_r = fopen(fn_r,"rb"))==NULL){
-               printf("can't open [%s]\n",fn_r);
-               perror("reason:");
-               exit(1);
-            }
-         }else{
-            fp_r = stdin;
-         }
-         mode[0]='w';
-         mode[1] = '0' + level;
-         mode[2] = '\0';
-
-         if((fn_w == NULL && (BZ2fp_w = BZ2_bzdopen(fileno(stdout),mode))==NULL)
-            || (fn_w !=NULL && (BZ2fp_w = BZ2_bzopen(fn_w,mode))==NULL)){
-            printf("can't bz2openstream\n");
-            exit(1);
-         }
-         while((len=fread(buff,1,0x1000,fp_r))>0){
-            BZ2_bzwrite(BZ2fp_w,buff,len);
-         }
-         BZ2_bzclose(BZ2fp_w);
-         if(fp_r!=stdin)fclose(fp_r);
+         BZ2_bzWriteClose(&bzerr, bzfp_w, 0, NULL, NULL);
+         if (bzerr != BZ_OK) fail_bzcall("BZ2_bzWriteClose", bzerr);
+         if (fp_r != stdin) fclose(fp_r);
+         if (fp_w != stdout) fclose(fp_w);
       }
    }
 #ifdef _WIN32
